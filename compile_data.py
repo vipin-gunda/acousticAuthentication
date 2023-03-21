38f8b1a9b2d8b0b9de0b8410144da7d953294a92
jc2627 <jonna7chen@gmail.com>
2023-03-21 15:35:34 -0400

    added training and testing label file func

--- compile_data.py
+++ compile_data.py
@@ -3,8 +3,9 @@ import random
 import json
 import os
 
-# ADJUSTABLE: PARTICIPANT NUMBER
+# ADJUSTABLE: PARTICIPANT NUMBER + EXPRESSION INDEX
 participant_number = 1
+expression_index = 0
 #participant_path contains the location of all the sessions for the participant in question.
 participant_path = '/data/shawn/authen_acoustic_2023sp/smart_eyewear_user_study/glasses_P'+ str(participant_number) +'_sitting'
 
@@ -60,19 +61,16 @@ for i in range(group_one_count + 1, total_sessions + 1):
            expression_map[exp][i-1].append(npy_index)
 
 # PART 2: READING HASHMAP TO CREATE NPY TRAINING DATA
-# ADJUSTABLE: Set correct expression you're trying to detect here
-expression_index = 0
 training_data = []
+training_labels = []
 testing_data = []
-testing_answers = []
-testing_expressions = []
+testing_labels = []
 
 npy_file_path = '/data/smart_eyewear/user_study/glasses_P' + str(participant_number) + '_sitting/'
 npy_1 = np.load(npy_file_path + 'facial_expression_1_fmcw_diff_CIR.npy')
 npy_2 = np.load(npy_file_path + 'facial_expression_2_fmcw_diff_CIR.npy')
 duration = int(2.5 * 50000 / 600)
 
-# session #1-12
 def get_npy_frame(ind: int, session: int):
    shift_seconds = random.uniform(0, 0.5) # randomly generation a float between 0 to 0.5, shift in seconds
    shift_scaled = int(shift_seconds * 50000/600)
@@ -81,50 +79,48 @@ def get_npy_frame(ind: int, session: int):
    else:
        return npy_2[:, ind + shift_scaled:ind + duration + shift_scaled + 1].flatten("F") #CHECK FLATTENING
 
+# Compiling Training Data
 for exp in range(0, 8 + 1):
    if exp != expression_index:
        random_session_number = random.randint(1, total_sessions-2)
        session_length = len(expression_map[exp][random_session_number-1])
        random_npy_index = expression_map[exp][random_session_number-1][random.randint(0, session_length-1)]
-       #np.append(training_data, get_npy_frame(random_npy_index, random_session_number))
        training_data.append(get_npy_frame(random_npy_index, random_session_number))
+       training_labels.append(-1)
    else:
        for session_number in range(1, total_sessions - 2 + 1):
                for npy_index in expression_map[exp][session_number - 1]:
-                       #np.append(training_data,get_npy_frame(npy_index, session_number))
-                       training_data.append(get_npy_frame(npy_index, session_number))
+                        training_labels.append(1)
+                        training_data.append(get_npy_frame(npy_index, session_number))
 
+# Compiling Testing Data
 for exp in range(0, 8 + 1):
    if exp != expression_index:
        random_session_number = random.randint(total_sessions - 2 + 1, total_sessions)
        session_length = len(expression_map[exp][random_session_number-1])
        random_npy_index = expression_map[exp][random_session_number-1][random.randint(0, session_length-1)]
-       #np.append(testing_answers, -1)
-       testing_answers.append(-1)
-       #np.append(testing_expressions, exp)
-       testing_expressions.append(exp)
-       #np.append(testing_data, get_npy_frame(random_npy_index, random_session_number))
+       testing_labels.append(-1)
        testing_data.append(get_npy_frame(random_npy_index, random_session_number))
    else:
        for session_number in range(total_sessions - 2 + 1, total_sessions + 1):
                for npy_index in expression_map[exp][session_number - 1]:
-                       #np.append(testing_answers,1)
-                       testing_answers.append(1)
-                       #np.append(testing_expressions, exp)
-                       testing_expressions.append(exp)
-                       #np.append(testing_data, get_npy_frame(npy_index, session_number))
+                       testing_labels.append(1)
                        testing_data.append(get_npy_frame(npy_index, session_number))
 
 # PART 3: WRITING TRAINING AND TESTING DATA TO DATA FOLDER
 training_data = np.array(training_data)
+training_labels = np.array(training_labels)
 testing_data = np.array(testing_data)
+testing_labels = np.array(testing_labels)
 
 if not os.path.exists('data'):
    os.makedirs('data') 
    
 with open("data/training_data_p" + str(participant_number) + "_e" + str(expression_index) + ".txt", "w") as txt:
- json.dump(training_data.tolist(), txt)
-
-
+    json.dump(training_data.tolist(), txt)
+with open("data/training_labels_p" + str(participant_number) + "_e" + str(expression_index) + ".txt", "w") as txt:
+    json.dump(training_labels.tolist(), txt)
 with open("data/testing_data_p" + str(participant_number) +"_e" + str(expression_index) + ".txt", "w") as txt:
- json.dump(testing_data.tolist(), txt)
+    json.dump(testing_data.tolist(), txt)
+with open("data/testing_labels_p" + str(participant_number) +"_e" + str(expression_index) + ".txt", "w") as txt:
+    json.dump(testing_labels.tolist(), txt)
--- oneClassSVM.py
+++ oneClassSVM.py
@@ -13,8 +13,9 @@ expression_index = 0
 # PART 1: PARSE CORRESPONDING DATA
 # IMPORTANT FOR DEV: Reading from raw compiled data currently
 # Later, need to get the CNN feature data (just changing the path)
-testing_data_path = 'data/testing_data_p'+str(participant_number)+"_e"+str(expression_index)+".txt"
 training_data_path = 'data/training_data_p'+str(participant_number)+"_e"+str(expression_index)+".txt"
+testing_data_path = 'data/testing_data_p'+str(participant_number)+"_e"+str(expression_index)+".txt"
+testing_labels_path = 'data/testing_labels_p'+str(participant_number)+"_e"+str(expression_index)+".txt"
 
 with open(training_data_path) as f:
     content = f.read()
@@ -25,10 +26,15 @@ with open(testing_data_path) as f:
     content = f.read()
     if content:
         testing_data = np.array(json.loads(content))
+        
+with open(testing_labels_path) as f:
+    content = f.read()
+    if content:
+        testing_labels = np.array(json.loads(content))
 
 #print(training_data) #set of training data
 #print(testing_data) #set of testing data
-#print(testing_answers) #set of answers (1 or 0)
+#print(testing_labels) #set of answers (1 or 0)
 #print(testing_expressions) #set of expressions (0...8)
 
 # PART 2: TRAIN SVM ON TRAINING DATA
@@ -37,6 +43,8 @@ authen.fit(training_data)
 
 # PART 3: TEST SVM ON TESTING DATA
 predictions = authen.predict(testing_data)
-#score = accuracy_score(testing_answers, predictions)
-#print(score)
+print("SVM Yes/No Expression Predictions")
 print(predictions)
+score = accuracy_score(testing_labels, predictions)
+print("SVM Accuracy for Participant " + str(participant_number) + ", Expression " + str(expression_index))
+print(score)
